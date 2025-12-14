681682fd3a8f684146746872ed451b79d491f1b2
JTaguiamDev <jtaguiamwork@gmail.com>
2025-12-14 13:20:24 +0800

    Style: Replace alarm icon with new vector drawable
    Replaces the existing alarm icon (`ic_alarm_large`) with a new vector drawable (`reshot_icon_alarm_clock_rhw38p5sef.xml`) on the alarm screen. The tint attribute has also been removed from the `ImageView` in `activity_alarm.xml`.

--- app/src/main/java/com/cheermateapp/AlarmActivity.kt
+++ app/src/main/java/com/cheermateapp/AlarmActivity.kt
@@ -11,6 +11,7 @@ import android.os.Vibrator
 import android.view.View
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.cheermateapp.util.ReminderManager
